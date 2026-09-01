f441ac60d70c28eaf0f0c53e9d2af51185518f87
kleberson154 <kleberson55@hotmail.com>
2026-09-01 02:41:21 -0300

    feat: add axios interceptor for attaching authorization token to requests

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,17 @@
 import axios from 'axios';
 
+import { getToken } from '../utils/token';
+
 export const api = axios.create({
   baseURL: 'http://localhost:8080/api',
 });
+
+api.interceptors.request.use((config) => {
+  const token = getToken();
+
+  if(token){
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+})
