a7c804f4121e199a5cc2cdd02c2a5011b5bc5737
kleberson154 <kleberson55@hotmail.com>
2026-09-03 15:52:53 -0300

    feat: update API base URL to use environment variable for better configuration

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,7 +3,9 @@ import axios from 'axios';
 import { getToken, removeToken } from '../utils/token';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL:
+    import.meta.env.VITE_API_URL ??
+    'http://localhost:8080/api',
 });
 
 api.interceptors.request.use((config) => {
