132efdc868518a54bc3bda7777003067f252a4a7
kleberson154 <kleberson55@hotmail.com>
2026-09-02 22:38:19 -0300

    feat: enhance API response handling to remove token on 401/403 errors

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { getToken } from '../utils/token';
+import { getToken, removeToken } from '../utils/token';
 
 export const api = axios.create({
   baseURL: 'http://localhost:8080/api',
@@ -9,9 +9,27 @@ export const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = getToken();
 
-  if(token){
+  if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
-})
+});
+
+api.interceptors.response.use(
+  (response) => response,
+
+  (error) => {
+    const status = error.response?.status;
+
+    if (status === 401 || status === 403) {
+      removeToken();
+
+      if (window.location.pathname !== '/login') {
+        window.location.href = '/login';
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
